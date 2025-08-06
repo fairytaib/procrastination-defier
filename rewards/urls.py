71d4cff8a73d6d89f47c6b49c1d66fa0d1a6e31d
fairytaib <fairytaib@gmail.com>
2025-08-06 17:11:34 +0200

    Set up reward view

--- rewards/urls.py
+++ rewards/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from . import views
 from django.urls import path, include
 
 urlpatterns = [
--- rewards/views.py
+++ rewards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Reward
 
+
 def rewards_list(request):
     """View to display a list of rewards."""
     rewards = Reward.objects.all()
