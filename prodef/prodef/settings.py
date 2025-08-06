9ec308a9d16902605659da13b5dda456c8311b0e
fairytaib <fairytaib@gmail.com>
2025-08-06 10:50:20 +0200

    Appy and install django.allauth

--- prodef/prodef/settings.py
+++ prodef/prodef/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'tasks',
     'rewards',
 ]
@@ -74,6 +78,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    ]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'prodef.wsgi.application'
 
