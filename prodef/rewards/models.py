9cd2d2dbddbebc28e1e8d0acdea81c08731f5cd2
fairytaib <fairytaib@gmail.com>
2025-08-06 14:55:04 +0200

    Create Rewards model

--- prodef/rewards/models.py
+++ prodef/rewards/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from multiselectfield import MultiSelectField
+import pycountry
 
-# Create your models here.
+REWARD_TYPES = [
+    ('digital', 'Digital'),
+    ('discount', 'Discount Code'),
+    ('travel', 'Travel'),
+    ('wellness', 'Wellness'),
+    ('food', 'Food'),
+    ('entertainment', 'Entertainment'),
+    ('other', 'Other'),
+]
+
+COUNTRY_CHOICES = [
+    (country.alpha_2, country.name) for country in pycountry.countries
+]
+
+
+class Reward(models.Model):
+    """A reward model to represent the rewards in the system."""
+
+    name = models.CharField(max_length=100, unique=True)
+    image = CloudinaryField(
+        'image', default='placeholder',
+        blank=True, null=True)
+    description = models.TextField()
+    cost = models.PositiveIntegerField(
+        help_text="Cost in points to redeem this reward.", default=1000)
+    stock = models.PositiveIntegerField(
+        help_text="Number of items available for this reward.", default=0)
+    reward_type = models.CharField(
+        choices=REWARD_TYPES,
+        max_length=50,
+        verbose_name="Reward Type",
+    )
+    available_countries = MultiSelectField(
+        choices=COUNTRY_CHOICES,
+        blank=True,
+        null=True,
+        verbose_name="Reward available in Countries",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
