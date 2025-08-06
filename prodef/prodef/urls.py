dd5e51632dbdc457761cf5da45129bde4bfb3646
fairytaib <fairytaib@gmail.com>
2025-08-06 12:19:14 +0200

    Database was fixed - wrong migration order

--- prodef/prodef/urls.py
+++ prodef/prodef/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
