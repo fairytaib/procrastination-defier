448653f5e4c78ff6cbfc26c6446efbba3f8b7a99
fairytaib <fairytaib@gmail.com>
2025-08-06 17:41:21 +0200

    Create own reward_detail page

--- rewards/urls.py
+++ rewards/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.rewards_list, name='rewards_list'),
+    path('reward_details/<int:reward_id>/',
+         views.view_details,
+         name='reward_details'),
 ]
--- rewards/views.py
+++ rewards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Reward
 
 
@@ -6,3 +6,15 @@ def rewards_list(request):
     """View to display a list of rewards."""
     rewards = Reward.objects.all()
     return render(request, 'rewards/rewards_list.html', {'rewards': rewards})
+
+
+def view_details(request, reward_id):
+    """Display the details of a reward."""
+    reward = get_object_or_404(Reward, id=reward_id)
+
+    context = {
+        "reward": reward,
+        "reward_id": reward_id,
+    }
+
+    return render(request, "rewards/view_reward_details.html", context)
