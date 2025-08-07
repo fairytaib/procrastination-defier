7817691bf4036c0dc05438b30476c203bc66041d
fairytaib <fairytaib@gmail.com>
2025-08-07 08:56:17 +0200

    Create User Tas overview and url

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Task, Task_Checkup
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def user_task_overview(request):
     """Render a user's task overview page."""
-    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
+    tasks = Task.objects.filter(user=request.user, completed=False).order_by('-created_at')
     context = {
         'tasks': tasks,
     }
-    return render(request, 'tasks/task_overview.html', context)
+    return render(request, 'tasks/user_task_overview.html', context)
