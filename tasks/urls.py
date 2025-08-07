c2310ded85b863caebcdee8979a7e565af8f7229
fairytaib <fairytaib@gmail.com>
2025-08-07 09:16:05 +0200

    Add Detail-view for tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.user_task_overview, name='user_task_overview'),
+    path('task_details/', views.view_task_details, name='view_task_details'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task, Task_Checkup
 from django.contrib.auth.decorators import login_required
 
@@ -6,8 +6,23 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def user_task_overview(request):
     """Render a user's task overview page."""
-    tasks = Task.objects.filter(user=request.user, completed=False).order_by('-created_at')
+    tasks = Task.objects.filter(
+        user=request.user, completed=False).order_by('-created_at')
     context = {
         'tasks': tasks,
     }
     return render(request, 'tasks/user_task_overview.html', context)
+
+
+def view_task_details(request, task_id):
+    """Display the details of a task."""
+    task = get_object_or_404(Task, id=task_id)
+    task_checkup = Task_Checkup.objects.filter(task=task).first()
+
+    context = {
+        "task": task,
+        "task_id": task_id,
+        "task_checkup": task_checkup,
+    }
+
+    return render(request, "tasks/view_task_details.html", context)
