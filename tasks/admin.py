c0a4871af53000c13cc6c789e6d7b91c08fb482c
fairytaib <fairytaib@gmail.com>
2025-08-07 16:29:00 +0200

    Position Dropdown menu properly

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Task, Task_Checkup
+from .models import Task, Task_Checkup, UserPoints
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(Task_Checkup)
+admin.site.register(UserPoints)
