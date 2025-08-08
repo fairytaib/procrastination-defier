0b12f68c0899218c8710e84101217a67c945e6e6
fairytaib <fairytaib@gmail.com>
2025-08-08 11:00:07 +0200

    Connect custom 404 and 500 sites to django

--- prodef/urls.py
+++ prodef/urls.py
@@ -17,6 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.conf.urls import handler404, handler500
+
+
+def custom_404(request, exception):
+    return render(request, "404.html", status=404)
+
+
+def custom_500(request):
+    return render(request, "500.html", status=500)
+
+handler404 = "prodef.urls.custom_404"
+handler500 = "prodef.urls.custom_500"
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
