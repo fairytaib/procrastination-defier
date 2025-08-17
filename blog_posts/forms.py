10b18acd9e507867c9cee73fb23bac18b98ae013
fairytaib <fairytaib@gmail.com>
2025-08-17 18:40:10 +0200

    Add comment form to posts

--- blog_posts/forms.py
+++ blog_posts/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from .models import Comment
 
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Add a comment...'}),
+        }
--- blog_posts/views.py
+++ blog_posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
+from .forms import CommentForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def view_all_posts(request):
@@ -10,7 +13,27 @@ def view_all_posts(request):
 def view_details(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = Comment.objects.filter(post=post)
+    comment_form = CommentForm()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.success(
+                    request, "Comment posted successfully."
+                    )
+            return redirect("view_details", post_id=post.id)
+    
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": comment_form
+
+    }
+
     return render(
         request, "blog_posts/post_details.html",
-        {"post": post, "comments": comments}
+        context
     )
