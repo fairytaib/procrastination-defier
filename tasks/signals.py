258a62bf9e904f122bfe39e969eab2841078c4e2
fairytaib <fairytaib@gmail.com>
2025-08-17 21:36:14 +0200

    Fix worng sender in signals function

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserPoints, Task_Checkup
+from .models import UserPoints, Task_Checkup, Task
 
 
 @receiver(post_save, sender=User)
@@ -13,7 +13,7 @@ def create_user_points(sender, instance, created, **kwargs):
         )
 
 
-@receiver(post_save, sender=UserPoints)
+@receiver(post_save, sender=Task)
 def connect_task_to_checkup(sender, instance, created, **kwargs):
     if created:
         Task_Checkup.objects.create(
