06ccfc864a7479d4fcd3b02fe5f15e315c71088f
fairytaib <fairytaib@gmail.com>
2025-08-27 22:35:47 +0200

    Safety commit

--- checkout/views.py
+++ checkout/views.py
@@ -2,10 +2,12 @@ from pyexpat.errors import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import OrderForm
 from rewards.models import Reward
+from tasks.models import UserPoints
 
 
 def place_order(request, reward_id):
     reward = get_object_or_404(Reward, id=reward_id)
+    user_points = get_object_or_404(UserPoints, user=request.user)
     if request.method == "POST":
         form = OrderForm(request.POST)
         if form.is_valid():
@@ -18,5 +20,6 @@ def place_order(request, reward_id):
         form = OrderForm()
     return render(
         request, "checkout/checkout.html", {
-            "form": form, "reward": reward}
+            "form": form, "reward": reward, "user_points": user_points
+        }
     )
