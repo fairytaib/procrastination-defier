90fd1dbead736f71e95011f702bc3ef6b14c63fd
fairytaib <fairytaib@gmail.com>
2025-08-30 00:10:46 +0200

    Connect urls to views

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,4 +3,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.subscription_view, name='subscription_view'),
+    path('create_subscription/',
+         views.create_subscription,
+         name='create_subscription'),
+    path(
+        'your_subscription/',
+        views.subscriptions_overview,
+        name='subscriptions_overview'
+        ),
 ]
