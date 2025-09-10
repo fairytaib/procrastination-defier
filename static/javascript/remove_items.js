c88b25078001c75767b865a4ce5c17d0176cf893
fairytaib <fairytaib@gmail.com>
2025-09-10 12:08:10 +0200

    Remove disc icons

--- static/javascript/remove_items.js
+++
@@ -1,7 +0,0 @@
-    const dropdownMenuButton = document.getElementById('dropdownMenuButton');
-    const userDropdown = document.getElementById('userDropdown');
-    const userDropdownMenu = document.getElementById('userDropdownMenu');
-
-    if (window.matchMedia('(max-width: 992px)').matches) {
-        dropdownMenuButton.style.display = 'none';
-    }
