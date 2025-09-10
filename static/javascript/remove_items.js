3dc1732a4b14c04e3d16d4b3298d67fec67ae5b7
fairytaib <fairytaib@gmail.com>
2025-09-10 11:09:01 +0200

    Move subscription into profile template

---
+++ static/javascript/remove_items.js
@@ -0,0 +1,7 @@
+    const dropdownMenuButton = document.getElementById('dropdownMenuButton');
+    const userDropdown = document.getElementById('userDropdown');
+    const userDropdownMenu = document.getElementById('userDropdownMenu');
+
+    if (window.matchMedia('(max-width: 992px)').matches) {
+        dropdownMenuButton.style.display = 'none';
+    }
